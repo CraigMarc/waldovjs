2a168e5193670e6a56040b2031d6e4cca987211c
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2024-01-04 21:20:31 -0500

    finished setting up webpack

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import './styles.css';
+import myName from './myName';
+
+function component() {
+    const element = document.createElement('div');
+  
+    // use your function!
+    element.textContent = myName('Craig');
+    return element;
+  }
+  
+  document.body.appendChild(component());
 
