72f87985f2eb06079dd76ed9bec47959c8735727
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2024-01-11 19:33:27 -0500

    got the circles in the right position

--- src/index.js
+++ src/index.js
@@ -23,36 +23,18 @@ pic.addEventListener('click', imageCoor)
 //get coordinate of click
 
 function imageCoor(e) {
-  //console.log(e.pageX)
-  //console.log(e.pageY)
-  let coord = [e.pageX, e.pageY]
+  
+  let coord = [e.offsetX, e.offsetY]
   addCircle(coord)
 }
 
 const addCircle = (coord) => {
   // get click coordinates
   let [x, y] = coord
-  //console.log(x)
-  // console.log(y)
-/*
-  const element = document.createElement('div');
-  const circle = document.createElement('circle');
-  circle.setAttribute('cx', x)
-  circle.setAttribute('cy', y)
-  circle.setAttribute('r', "10")
-  circle.setAttribute('stroke', "black")
-  circle.setAttribute('strokeWidth', "1")
-  circle.setAttribute('fill', "red")
-  element.appendChild(circle);
-  const menuDescr = document.createElement('p');
-  menuDescr.textContent = "pizzaText"
-  element.appendChild(menuDescr);
-  return document.body.appendChild(element);
-*/
+ 
 
 const element = document.getElementById('container');
 
-const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
 const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
 
 circle.setAttribute('cx', x)
@@ -62,10 +44,7 @@ circle.setAttribute('stroke', "black")
 circle.setAttribute('stroke-width', "3")
 circle.setAttribute('fill', "none")
 
-svg.appendChild(circle)
-element.appendChild(svg);
-
-//document.body.appendChild(element)
+element.appendChild(circle)
 
   
 }
