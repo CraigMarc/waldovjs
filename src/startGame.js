4ff0619cc708bdca33f78e873523434cf896c5fc
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2024-01-18 13:52:10 -0500

    added pic

--- src/startGame.js
+++ src/startGame.js
@@ -1,11 +1,17 @@
+import waldo2 from './images/waldo2resize.jpg'
+
 const startGame = () => {
 
-    
+
     const startContainer = document.createElement('div');
     startContainer.id = "buttonContainer"
+    const image1 = new Image();
+    image1.src = waldo2;
+    image1.classList.add('waldo1Pic');
     const button = document.createElement('button');
     button.textContent = 'Start Game';
     button.id = 'button'
+    startContainer.appendChild(image1)
     startContainer.appendChild(button)
     document.body.appendChild(startContainer);
 }
