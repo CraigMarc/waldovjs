13c7e3ef5b93c3299b34ca1470a74cf9e42d4708
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2024-01-18 13:57:10 -0500

    added pic to start screen

--- src/startGame.js
+++ src/startGame.js
@@ -5,12 +5,15 @@ const startGame = () => {
 
     const startContainer = document.createElement('div');
     startContainer.id = "buttonContainer"
+    const heading = document.createElement('h1');
+    heading.textContent = "Where's Waldo";
     const image1 = new Image();
     image1.src = waldo2;
     image1.classList.add('waldo1Pic');
     const button = document.createElement('button');
     button.textContent = 'Start Game';
     button.id = 'button'
+    startContainer.appendChild(heading)
     startContainer.appendChild(image1)
     startContainer.appendChild(button)
     document.body.appendChild(startContainer);
