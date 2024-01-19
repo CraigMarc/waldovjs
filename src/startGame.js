8bfb34d4a60f6076c36a2d21026c89f57420291a
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2024-01-19 13:34:36 -0500

    added new boards to start screen

--- src/startGame.js
+++ src/startGame.js
@@ -1,9 +1,15 @@
+import waldo0 from './images/waldo0resize.jpg'
+import waldo1 from './images/waldo1resize.jpg'
 import waldo2 from './images/waldo2resize.jpg'
+import waldo3 from './images/waldo3resize.jpg'
 import waldo from './images/waldoHead.jpg'
 import wenda from './images/wendaHead.jpg'
 import wizard from './images/wizardHead.jpg'
 
 const startGame = () => {
+
+    let imageArray = [waldo0, waldo1, waldo2, waldo3]
+    let titleArray = ["The Beach", "Troy", "Maze", "Robin Hood"]
 
 
     const startContainer = document.createElement('div');
@@ -20,25 +26,37 @@ const startGame = () => {
     const headImage3 = new Image();
     headImage3.src = wizard;
     headImage3.classList.add('headImage');
-    const picHeaderContainer = document.createElement('div');
-    picHeaderContainer.textContent = "The Beach"
-    const imageContainer = document.createElement('div');
-    imageContainer.id = "imageContainer"
-    const image1 = new Image();
-    image1.src = waldo2;
-    image1.classList.add('waldo1Pic');
-    const button = document.createElement('button');
-    button.textContent = 'Start Game';
-    button.id = 'button'
-    imageContainer.appendChild(image1)
+
+
     headingImages.appendChild(headImage1)
     headingImages.appendChild(headImage2)
     headingImages.appendChild(headImage3)
     startContainer.appendChild(heading)
     startContainer.appendChild(headingImages)
-    startContainer.appendChild(picHeaderContainer)
-    startContainer.appendChild(imageContainer)
-    startContainer.appendChild(button)
+
+    // game boards
+
+    for (let i = 0; i < imageArray.length; i++) {
+
+        const picHeaderContainer = document.createElement('div');
+        picHeaderContainer.textContent = titleArray[i]
+        const imageContainer = document.createElement('div');
+        imageContainer.id = "imageContainer"
+        const image0 = new Image();
+        image0.src = imageArray[i];
+        image0.classList.add('waldo1Pic');
+        const button = document.createElement('button');
+        button.textContent = 'Start Game';
+        button.id = 'button'
+
+        imageContainer.appendChild(image0)
+        startContainer.appendChild(picHeaderContainer)
+        startContainer.appendChild(imageContainer)
+        startContainer.appendChild(button)
+
+    }
+
+
     document.body.appendChild(startContainer);
 }
 
