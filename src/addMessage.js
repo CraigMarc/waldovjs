f6ce6c410ca1dc157c6ff634f05d19232bf0affb
craigmarc <cmarcinkiewicz2000@yahoo.com>
2026-02-18 12:14:23 -0500

    got rid of console ran webpack build

--- src/addMessage.js
+++ src/addMessage.js
@@ -3,8 +3,7 @@
 const addMessage = (message, name) => {
 
     let messageName = name.charAt(0).toUpperCase + name.slice(1)
-    
-  
+   
     const element = document.createElement('div');
     element.id = 'message';
     if (message == false) {
