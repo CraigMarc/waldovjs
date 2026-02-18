1385317f3222ac70dd59d2c684ff5161d2d05036
craigmarc <cmarcinkiewicz2000@yahoo.com>
2026-02-18 11:44:17 -0500

    capitalized first letter in message name

--- src/addMessage.js
+++ src/addMessage.js
@@ -1,8 +1,9 @@
 // add message
 
 const addMessage = (message, name) => {
+
+    let messageName = name.charAt(0).toUpperCase + name.slice(1)
   
- 
     const element = document.createElement('div');
     element.id = 'message';
     if (message == false) {
@@ -10,7 +11,7 @@ const addMessage = (message, name) => {
       element.style.backgroundColor = 'red'
     }
     if (message == true) {
-      element.textContent = `You Found ${name}`
+      element.textContent = `You Found ${messageName}`
       element.style.backgroundColor = 'green'
     }
     element.classList.add('message');
